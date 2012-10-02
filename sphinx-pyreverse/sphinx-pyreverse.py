2f1de84a10ac23b9007d0b95e9622d083cd74aa8
Dmytro Vorona <alendit@gmail.com>
2012-10-02 15:32:09 +0200

    Debuggin code removed

--- sphinx-pyreverse/sphinx-pyreverse.py
+++ sphinx-pyreverse/sphinx-pyreverse.py
@@ -9,12 +9,7 @@ from sphinx.util.compat import Directive
 from sphinx.util.compat import make_admonition
 from subprocess import call
 import os
-import shutil
-import tempfile
 
-
-class UMLDiagramm(nodes.General, nodes.Element):
-    pass
 
 class UMLGenerateDirective(Directive):
     required_arguments = 1
@@ -26,22 +21,17 @@ class UMLGenerateDirective(Directive):
         SRC_DIR = env.srcdir
         DIR_NAME = "uml_images"
         UML_DIR = os.path.join(SRC_DIR, DIR_NAME)
+        
         if os.path.basename(UML_DIR) not in os.listdir(SRC_DIR):
             os.mkdir(UML_DIR)
         env.uml_dir = UML_DIR
         module_path = self.arguments[0]
-        print "#" * 10
-        print module_path
-        print UML_DIR
-        old_dir = os.getcwd()
         os.chdir(UML_DIR)
         basename = os.path.basename(module_path).split(".")[0]
-        print call(['pyreverse', '-o', 'png', '-p', basename, os.path.abspath(os.path.join(old_dir, module_path))])
         
         uri = directives.uri(os.path.join(DIR_NAME, "classes_{0}.png".format(basename)))
-        print uri
         img = nodes.image(uri=uri)
-        os.chdir(old_dir)
+        os.chdir(SRC_DIR)
         return [img]
 
 def visit_uml_node(self, node):
