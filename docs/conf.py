a6374288a2059db666e9c366539bca84f8a35a67
Dmytro Vorona <alendit@gmail.com>
2012-10-02 09:13:32 +0200

    Some structural rework

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath(".."))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -27,7 +27,7 @@ sys.path.append(os.path.abspath("."))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['todo']
+extensions = ['sphinx-pyreverse']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
