5a6cbefafeb351c1b1f5d2316318d4b4c903d1ed
Frank Harrison <frank@doublethefish.com>
2019-10-08 14:55:23 +0100

    Applies python-black

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
-'''
+"""
 Created on Oct 2, 2012
 
 @author: alendit
-'''
+"""
 from setuptools import setup
 import os
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name="sphinx-pyreverse",
-      version="0.0.10",
-      author="Dimitri Vorona",
-      author_email="vorona@in.tum.de",
-      description=("A simple sphinx extension to generate "
-                    "UML diagrams with pyreverse"),
-      license="GPLv3",
-      keywords="sphinx extension uml pyreverse",
-      url="https://github.com/alendit/sphinx-pyreverse",
-      long_description=read('README.rst'),
-      packages=['sphinx_pyreverse'],
-      install_requires=['sphinx', 'docutils'],
-      classifiers=[
-                   'Development Status :: 2 - Pre-Alpha',
-                   'Intended Audience :: Information Technology',
-                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities',
-                   ]
-      )
+
+setup(
+    name="sphinx-pyreverse",
+    version="0.0.10",
+    author="Dimitri Vorona",
+    author_email="vorona@in.tum.de",
+    description=(
+        "A simple sphinx extension to generate " "UML diagrams with pyreverse"
+    ),
+    license="GPLv3",
+    keywords="sphinx extension uml pyreverse",
+    url="https://github.com/alendit/sphinx-pyreverse",
+    long_description=read("README.rst"),
+    packages=["sphinx_pyreverse"],
+    install_requires=["sphinx", "docutils"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
