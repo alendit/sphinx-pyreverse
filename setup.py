dc0870caff29e3e406764637f92ce871d0c85008
Frank Harrison <frank@doublethefish.com>
2019-10-09 01:31:42 +0100

    Sets version pre-release alpha number, that works with the pypi store
    This is so we can iterate several times, safely, without incrementing
    the patch number everytime.
    
    The idea is we do alphas, betas, release-candidates and the final release - well, we'll
    probably do an alpha and then the release.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
 
 setup(
     name="sphinx-pyreverse",
-    version="0.0.10",
+    version="0.0.11a001",
     author="Dimitri Vorona",
     author_email="vorona@in.tum.de",
     description=(
